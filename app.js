5a9769f5b794c54e41c102d9cfc411ca2c276aac
Elise Wetherholt <ewether@gmail.com>
2021-02-25 08:52:56 -0500

    made li elements clickable, changed colors and added new font

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-let container = document.querySelector('.container');
-let onLoadText = document.querySelector('.on-load-text');
-let options = document.querySelector('.options-wrapper');
-let timeOpt = document.querySelector('.time-opt');
-let musicOpt = document.querySelector('.music-opt');
+$(document).ready(function(){
 
-setTimeout(function() {
-    onLoadText.style.opacity = "0";
-}, 3200);
+    let container = document.querySelector('.container');
+    let onLoadText = document.querySelector('.on-load-text');
+    let options = document.querySelector('.options-wrapper');
+    let timeOpt = $(".time-opt");
+    let musicOpt = document.querySelector('.music-opt');
 
-setTimeout(function() {
-    onLoadText.style.display = "none";
-}, 5);
+    setTimeout(function() {
+        onLoadText.style.opacity = "0";
+    }, 3200);
+    // 3200
 
-setTimeout(function() {
-    options.style.opacity = "1";
-}, 6500);
+    setTimeout(function() {
+        onLoadText.style.display = "none";
+    }, 5);
+    // 5000
 
-timeOpt.addEventListener('click', function() {
-    timeOpt.style.backgroundColor = "#6e6668";
-    console.log(timeOpt);
+    setTimeout(function() {
+        options.style.opacity = "1";
+    }, 10);
+    // 5800
+
+    $('.time-options ul li').click(function() {
+        $(this).css("background-color", "#4d4c61");
+    });
+
+    // if you click another element, 
+    // -> background color goes back to normal on all other elements
+
+    $('.music-options ul li').click(function() {
+        $(this).css("background-color", "#4d4c61");
+    });
+
 });
